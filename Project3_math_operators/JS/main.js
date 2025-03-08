27c5d3a9519cbda45ea546530510501874646b5a
Erhan Yenigun <eyenigun@gmail.com>
2025-03-08 09:32:54 -0500

    Basic Java Script

---
+++ Project3_math_operators/JS/main.js
@@ -0,0 +1,60 @@
+
+function addition_Function () {
+  document.getElementById("Add").innerHTML = 2+2 ;
+}
+
+
+
+function subtract_Function () {
+  document.getElementById("Sub").innerHTML = 5-2 ;
+}
+
+
+function multiply_Function () {
+  document.getElementById("Mult").innerHTML = 5*8 ;
+}
+
+
+function devide_Function () {
+  document.getElementById("Div").innerHTML = 22/4 ;
+}
+
+
+function multiple_Function () {
+  document.getElementById("Multiple").innerHTML = 5-2*45 ;
+}
+
+
+function modulus_Function () {
+  document.getElementById("Mod").innerHTML = 114/23 ;
+}
+
+
+function unary_Function () {
+  var x=10;
+  document.getElementById("Unary").innerHTML = -x ;
+}
+
+
+function increment_Function () {
+  var x=10;
+  x++ ;
+  document.write(x) ;
+}
+
+
+function decrement_Function () {
+  var x=10;
+  x-- ;
+  document.write(x) ;
+}
+
+
+function random_Function () {
+  document.write(Math.random() *100) ;
+}
+
+
+
+
+
